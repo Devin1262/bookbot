cb15f61e6629ea7c4eb57c67ed7f3ec280ee27f7
Devin1262 <devintran58@gmail.com>
2024-11-21 20:15:32 -0500

    B: added functions to count and sort char's

--- main.py
+++ main.py
@@ -3,19 +3,32 @@ def main():
     text = get_text(filepath)
     number_of_words = count_words(text)
     number_of_char = count_characters(text)
-    print(text)
-    print(number_of_words)
-    print(number_of_char)
-    
-    
+    char_count = to_list(number_of_char)
+    print(f"--- Begin report of {filepath}")
+    print(f"{number_of_words} found in the document\n")
+
+    for obj in char_count:
+        ch, num = obj
+        print(f"The '{ch}' character was found {num} times")
+    print("--- End report ---")
+
+
+def to_list(dic):
+    lis = []
+    for key in dic:
+        lis.append((key, dic[key]))
+    lis.sort(reverse = True, key = lambda x:x[1])
+    return lis
+ 
 def count_characters(string):
     appearances = {}
     lower = string.lower()
     for char in lower:
-        if(char not in appearances):
-            appearances[char] = 1
-        else:
-            appearances[char] += 1
+        if(char.isalpha()):
+            if(char not in appearances):
+                appearances[char] = 1
+            else:
+                appearances[char] += 1
     return appearances
     
 def count_words(string):
